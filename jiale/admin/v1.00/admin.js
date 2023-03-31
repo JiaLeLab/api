bc866b6319f67bfd09d9a8c76489ecf924893875
Jia Le <jle26531@gmail.com>
2023-03-31 23:53:57 +0800

    Admin static v1

---
+++ jiale/admin/v1.00/admin.js
@@ -0,0 +1,53 @@
+// Prepared by Jia Le. >_<
+
+const sideMenu = document.querySelector("aside");
+const menuBtn = document.querySelector("#menu-btn");
+const closeBtn = document.querySelector("#close-btn");
+const themeToggler = document.querySelector('.theme-toggler');
+
+menuBtn.addEventListener('click', () => {
+    sideMenu.style.display = 'block';
+})
+
+closeBtn.addEventListener('click', () => {
+    sideMenu.style.display = 'none';
+})
+
+themeToggler.addEventListener('click', () => {
+    document.body.classList.toggle('dark-theme-variables');
+
+    themeToggler.querySelector('span:nth-child(1)').classList.toggle('active');
+    themeToggler.querySelector('span:nth-child(2)').classList.toggle('active');
+
+    if (document.body.classList.contains('dark-theme-variables')) {
+        localStorage.setItem('theme', 'dark');
+    } else {
+        localStorage.setItem('theme', 'light');
+    }
+});
+
+document.addEventListener('DOMContentLoaded', () => {
+    const sideMenu = document.querySelector("aside");
+    const menuBtn = document.querySelector("#menu-btn");
+    const closeBtn = document.querySelector("#close-btn");
+    const themeToggler = document.querySelector('.theme-toggler');
+
+    menuBtn.addEventListener('click', () => {
+        sideMenu.style.display = 'block';
+    })
+
+    closeBtn.addEventListener('click', () => {
+        sideMenu.style.display = 'none';
+    })
+
+    const theme = localStorage.getItem('theme');
+    if (theme === 'dark') {
+        document.body.classList.add('dark-theme-variables');
+        themeToggler.querySelector('span:nth-child(1)').classList.remove('active');
+        themeToggler.querySelector('span:nth-child(2)').classList.add('active');
+    } else {
+        document.body.classList.remove('dark-theme-variables');
+        themeToggler.querySelector('span:nth-child(1)').classList.add('active');
+        themeToggler.querySelector('span:nth-child(2)').classList.remove('active');
+    }
+});
